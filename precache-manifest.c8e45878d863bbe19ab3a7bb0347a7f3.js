fb8f430ea87820d0c16aea182c46aa3f802eaab2
matiasnauel <ayala.matias.ma@gmail.com>
2021-05-12 23:31:21 -0300

    Updates

---
+++ precache-manifest.c8e45878d863bbe19ab3a7bb0347a7f3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21c17d8c686f66c534449b4a96bdf5d9",
+    "url": "/cloud-unaj/index.html"
+  },
+  {
+    "revision": "a76e87f0a28be20f907c",
+    "url": "/cloud-unaj/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "a76e87f0a28be20f907c",
+    "url": "/cloud-unaj/static/js/2.fc9cadd9.chunk.js"
+  },
+  {
+    "revision": "6e86f9c5a5fae07a01b8c01faf571f12",
+    "url": "/cloud-unaj/static/js/2.fc9cadd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e8e0fa21224b49e8fe7",
+    "url": "/cloud-unaj/static/js/main.23f88725.chunk.js"
+  },
+  {
+    "revision": "4407094da090f94e8ba7",
+    "url": "/cloud-unaj/static/js/runtime-main.6063d848.js"
+  },
+  {
+    "revision": "0a875b3b500e3656816d7e0151b1f8de",
+    "url": "/cloud-unaj/static/media/UNAJ.0a875b3b.jpg"
+  }
+]);
